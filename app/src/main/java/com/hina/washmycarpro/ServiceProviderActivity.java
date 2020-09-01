a2d3f8ed631b4df02127d2bda98bc25957e96eb8
FaReenNaImRan <imranfareena66@gmail.com>
2020-09-02 00:21:31 +0500

    changes in menu bar

--- app/src/main/java/com/hina/washmycarpro/ServiceProviderActivity.java
+++ app/src/main/java/com/hina/washmycarpro/ServiceProviderActivity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 public class ServiceProviderActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider);
     }
